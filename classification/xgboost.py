7d9de8a4e99d0a5097d4a39b15ee5f79b8562ecc
Bosung-Yang <139629920+Bosung-Yang@users.noreply.github.com>
2023-09-21 09:16:16 +0900

    Create xgboost.py

---
+++ classification/xgboost.py
@@ -0,0 +1,41 @@
+import xgboost
+
+def init_model():
+  clf = xgboost.XGBClassifier(random_state = 42)
+  return clf
+
+def train(model, x, y):
+  clf.fit(x,y)
+  return clf
+
+from sklearn.metrics import *
+
+def evaluate(clf, x, y):
+  predicted = clf.predict(x)
+  print('[F1 score] : ',f1_score(y, pred, average='macro'))
+  print('[Acc] : ', accuracy_score(y, pred))
+  print('[Precision] : ', precision_score(y, pred, average='macro'))
+  print('[Recall] : ', recall_score(y,pred, average='macro'))
+
+import pickle
+
+def save_model(clf, fn): # clf : classification model, fn : file name
+  try:
+    with open(fn, 'wb') as f:
+      pickle.dump(clf, f)
+    print('[+] model saved {}'.format(f))
+    return True
+  except:
+    print('[!] error at save_model')
+    return False
+
+def load_model(fn):
+  try:
+    with open(fn, 'rb') as f:
+      clf = pickle.load(f)
+      print('[+] {} is loaded'.format(f))
+      return clf
+  except:
+    print('[!] error at load_model')
+    return False
+    
